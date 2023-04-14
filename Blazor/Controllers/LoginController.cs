cc5ffeaea0f0d56a90251e5d46a5379b87cd526d
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-04-13 23:28:48 -0600

    Web

---
+++ Blazor/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.Controller
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
